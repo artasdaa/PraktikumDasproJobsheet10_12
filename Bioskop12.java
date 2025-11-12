c92f0f64d9d8c8febf1c0fabbfc950016cf1549c
artasdaa <cellom293@gmail.com>
2025-11-12 12:08:32 +0700

    Soal Percobaan1 No 8

--- Bioskop12.java
+++ Bioskop12.java
@@ -15,9 +15,10 @@ public class Bioskop12 {
         System.out.printf("%s \t %s%n", penonton[2][0], penonton[2][1]);
         System.out.printf("%s \t %s%n", penonton[3][0], penonton[3][1]);
 
-        for (int i = 0; i < penonton.length; i++) {
-            System.out.println("Penonton pada baris ke-" + (i + 1) + ": " + 
-                               String.join(", ", penonton[i]));
+        System.out.print("Penonton pada baris ke-3: ");
+        for (String nama : penonton[2]) {
+            System.out.print(nama + " ");
         }
+        System.out.println();
     }
 }
