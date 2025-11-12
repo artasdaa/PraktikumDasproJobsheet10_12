1fae9c7d5c50718dc7d690380efbbac586d0ff39
artasdaa <cellom293@gmail.com>
2025-11-12 14:23:28 +0700

    Soal Percobaan4 No 1

--- SIAKAD12.java
+++ SIAKAD12.java
@@ -5,16 +5,28 @@ public class SIAKAD12 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[][] nilai = new int[4][3];
+
+        System.out.println("==========================================");
+        System.out.println("     PENGATURAN DATA SIAKAD DINAMIS       ");
+        System.out.println("==========================================");
         
+        System.out.print("Masukkan Jumlah Siswa: ");
+        int jumlahSiswa = sc.nextInt();
+        
+        System.out.print("Masukkan Jumlah Mata Kuliah: ");
+        int jumlahMatkul = sc.nextInt();
+        
+        int[][] nilai = new int[jumlahSiswa][jumlahMatkul];
+        
+        System.out.println("\n==========================================");
+        System.out.printf("   INPUT NILAI SIAKAD (%d SISWA, %d MK)   \n", jumlahSiswa, jumlahMatkul);
         System.out.println("==========================================");
-        System.out.println("       INPUT NILAI SIAKAD (4 SISWA)       ");
-        System.out.println("==========================================");
+        
         for (int i = 0; i < nilai.length; i++) {
             System.out.println("\nInput nilai mahasiswa ke-" + (i + 1) + ")");
             
             for (int j = 0; j < nilai[i].length; j++) {
-                System.out.print("Nilai mata kuliah " + (j + 1) + " (MK" + (j+1) + "): ");
+                System.out.print("Nilai mata kuliah ke-" + (j + 1) + ": ");
                 nilai[i][j] = sc.nextInt();
             }
         }
@@ -25,8 +37,9 @@ public class SIAKAD12 {
         }
         
         System.out.println("\n==========================================");
-        System.out.println("         HITUNG RATA-RATA SISWA           ");
+        System.out.println("          HITUNG RATA-RATA SISWA          ");
         System.out.println("==========================================");
+        
         for (int i = 0; i < nilai.length; i++) {
             double totalPerSiswa = 0;
             
@@ -35,32 +48,26 @@ public class SIAKAD12 {
                 totalPerSiswa += nilai[i][j]; 
             }
             
-            double rataRata = totalPerSiswa / 3.0; 
+            double rataRata = totalPerSiswa / jumlahMatkul; 
             
             System.out.println("Total Nilai: " + totalPerSiswa);
             System.out.printf("Nilai Rata-rata: %.2f\n", rataRata);
         }
         
         System.out.println("\n==========================================");
-        System.out.println("        HITUNG RATA-RATA MATA KULIAH      ");
+        System.out.println("       HITUNG RATA-RATA MATA KULIAH       ");
         System.out.println("==========================================");
         
-        for (int j = 0; j < 3; j++) {
+        for (int j = 0; j < jumlahMatkul; j++) {
             double totalPerMatkul = 0;
             
-            for (int i = 0; i < 4; i++) {
+            for (int i = 0; i < jumlahSiswa; i++) {
                 totalPerMatkul += nilai[i][j]; 
             }
             
-            double rataRataMatkul = totalPerMatkul / 4.0; 
-            String namaMatkul = switch (j) {
-                case 0 -> "Dasar Pemrograman";
-                case 1 -> "Matematika";
-                case 2 -> "CTPS";
-                default -> "Matkul Lain";
-            };
+            double rataRataMatkul = totalPerMatkul / jumlahSiswa; 
             
-            System.out.printf("Rata-rata Nilai Matkul %d (%s): %.2f\n", (j + 1), namaMatkul, rataRataMatkul);
+            System.out.printf("Rata-rata Nilai Matkul %d: %.2f\n", (j + 1), rataRataMatkul);
         }
         
         sc.close();
